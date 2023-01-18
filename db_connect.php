ca966cbfd3fff9177b1e59d1cf0cf6ea5c618db1
ronaldocc13 <67851747+ronalcc13@users.noreply.github.com>
2023-01-18 16:28:02 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('containers-us-west-134.railway.app', 'root', 'GSlGysnHsmSCDy55etkH', 'railway');
+	$con = mysqli_connect('containers-us-west-134.railway.app', 'root', 'GSlGysnHsmSCDy55etkH', 'railway',8069);
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>

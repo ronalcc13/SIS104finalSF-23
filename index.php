7a22cce1d514b6e48ef6d152493584bb6d4e613f
ronaldocc13 <67851747+ronalcc13@users.noreply.github.com>
2023-01-18 16:02:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,21 +6,6 @@
 		<link rel="stylesheet" type="text/css" href="css/index_style.css" />
 	</head>
 	<body>
-		<div id="allTheThings">
-			<div id="member">
-				<a href="1Visor">
-					<img src="img/ic_member.svg" width="250px" height="auto"/><br />
-					&nbsp;Miembro
-				</a>
-			</div>
-			<div id="verticalLine">
-				<div id="librarian">
-					<a  href="0Admin">
-					<img src="img/ic_member.svg" width="250px" height="auto" /><br />
-					&nbsp;&nbsp;&nbsp;Administración
-					</a>
-				</div>
-			</div>
-		</div>
+		<p>hola</p>
 	</body>
 </html>

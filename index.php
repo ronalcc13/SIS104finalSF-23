6fefb35af7988094ccfe06cd878d55baf98e101e
ronaldocc13 <67851747+ronalcc13@users.noreply.github.com>
2023-01-18 16:03:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+	require "db_connect.php";
+	require "header-index.php";
+	session_start();
+	
+	if(empty($_SESSION['type']));
+	else if(strcmp($_SESSION['type'], "admin") == 0)
+		header("Location: librarian/home.php");
+	else if(strcmp($_SESSION['type'], "visor") == 0)
+		header("Location: member/home.php");
+?>
 
 
 <html>
@@ -6,6 +17,21 @@
 		<link rel="stylesheet" type="text/css" href="css/index_style.css" />
 	</head>
 	<body>
-		<p>hola</p>
+		<div id="allTheThings">
+			<div id="member">
+				<a href="1Visor">
+					<img src="img/ic_member.svg" width="250px" height="auto"/><br />
+					&nbsp;Miembro
+				</a>
+			</div>
+			<div id="verticalLine">
+				<div id="librarian">
+					<a  href="0Admin">
+					<img src="img/ic_member.svg" width="250px" height="auto" /><br />
+					&nbsp;&nbsp;&nbsp;Administración
+					</a>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>

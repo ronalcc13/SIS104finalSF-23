7fb4665cbb77ba1176d361ccb050ea2044d04889
ronaldocc13 <67851747+ronalcc13@users.noreply.github.com>
2023-01-18 15:24:51 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('localhost', 'root', '', 'almacen_db');
+	$con = mysqli_connect('containers-us-west-121.railway.app', 'root', 'UqcOXIpmKzzYQV8FCcuk', 'railway');
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>
